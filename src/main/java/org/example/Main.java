9f6598c006165e7a689417c71197848b564e8fae
mastasalami <130776746+mastasalami@users.noreply.github.com>
2023-04-21 14:25:22 +0200

    Added hacky jsoup search

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,45 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Enter the URL you would like to analyse:");
+
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        try {
+            String url = reader.readLine();
+
+            System.out.println("Enter the search depth:");
+            String depth = reader.readLine();
+
+            System.out.println("Enter the language:");
+            String languageCode = reader.readLine();
+
+            Connection connection = Jsoup.connect(url);
+            Document document = connection.execute().parse();
+
+            List<Element> links = document.getElementsByTag("a");
+
+            for (Element link : links) {
+                String linkTo = link.attr("href");
+                String linkText = link.text();
+
+                System.out.println("linkText:" + linkText);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
