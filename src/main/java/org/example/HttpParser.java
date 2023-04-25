3bbfb81344096925e63a19a31ea1b8511884781a
fabianberchtold <127214759+fabianberchtold@users.noreply.github.com>
2023-04-25 20:08:19 +0200

    change visibility of methods

--- src/main/java/org/example/HttpParser.java
+++ src/main/java/org/example/HttpParser.java
@@ -21,10 +21,9 @@ public class HttpParser {
         return parser;
     }
 
-    String parseHttpResponse(HttpResponse<String> response, String jsonObjectKey) {
+  public String parseHttpResponse(HttpResponse<String> response, String jsonObjectKey) {
         JSONArray responseJson = extractJsonArrayFromHttpResponse(response);
         StringBuilder parsedResponse = new StringBuilder();
-        //Maybe extract method
         for (int i = 0; i < responseJson.length(); i++) {
             JSONObject responseObject = responseJson.getJSONObject(i);
             String parsed = responseObject.getString(jsonObjectKey);
@@ -34,9 +33,10 @@ public class HttpParser {
         return parsedString;
     }
 
-    JSONArray extractJsonArrayFromHttpResponse(HttpResponse<String> response) {
+    private JSONArray extractJsonArrayFromHttpResponse(HttpResponse<String> response) {
         String responseBody = response.body();
-        //TODO write comment
+        //In the HttpResponse of the Google Translate API wraps the JSON Array into something else. It also wraps the JSON array inside
+        // another JSON array for detect Requests. That's why I cut off part of the responseBody with lastIndexof('[')
         int jsonArrayStartIndex = responseBody.lastIndexOf('[');
         String extracted = responseBody.substring(jsonArrayStartIndex);
         return new JSONArray(extracted);
